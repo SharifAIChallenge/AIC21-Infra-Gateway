2c427bbc86efeaf1290c53c7713eaf39ff260815
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-29 14:29:16 +0430

    round robin

--- apps/game/views.py
+++ apps/game/views.py
@@ -2,11 +2,17 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from services.kafka_cli import KafkaClient
-from gateway.settings import KAFKA_TOPIC_MATCH_0, KAFKA_TOPIC_MATCH_1
+from gateway.settings import KAFKA_TOPIC_MATCH_0, KAFKA_TOPIC_MATCH_1, KAFKA_TOPIC_MATCH_0_NUM_PARTITIONS, \
+    KAFKA_TOPIC_MATCH_1_NUM_PARTITIONS
 
 from .serializers import GameRegisterSerializer
 import uuid
 from apps import permissions
+
+
+def next_partition(cur, total):
+    cur = (cur + 1) % total
+    return cur
 
 
 class PlayGameAPIView(GenericAPIView):
@@ -14,16 +20,16 @@ class PlayGameAPIView(GenericAPIView):
     serializer_class = GameRegisterSerializer
 
     def post(self, request):
-        topic = KAFKA_TOPIC_MATCH_0
         priority = request.GET.get('priority', '-1')
         if priority.isnumeric():
             priority = int(priority)
-            if priority == 1:
-                topic = KAFKA_TOPIC_MATCH_1
+            if priority != 1:
+                priority = 0
+
         game_id = uuid.uuid4()
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         game_information = serializer.data
         game_information['game_id'] = str(game_id)
-        KafkaClient.send_message(topic, game_information)
+        KafkaClient.send_message(priority, game_information)
         return Response(data={'game_id': game_id}, status=status.HTTP_200_OK)
--- services/kafka_cli.py
+++ services/kafka_cli.py
@@ -9,6 +9,22 @@ kafka_producer = KafkaProducer(
 )
 
 
+class Topic:
+    def __init__(self, topic_name, total_partitions):
+        self.total_partitions = total_partitions
+        self.cur = 0
+        self.topic_name = topic_name
+
+    def get_partition(self):
+        self.cur = (self.cur + 1) % self.total_partitions
+        return self.cur
+
+
+arenas = [Topic(os.getenv("KAFKA_TOPIC_MATCH_0"), os.getenv("KAFKA_TOPIC_MATCH_0_NUM_PARTITIONS")),
+          Topic(os.getenv("KAFKA_TOPIC_MATCH_1"), os.getenv("KAFKA_TOPIC_MATCH_1_NUM_PARTITIONS")),
+          ]
+
+
 class Topics(enum.Enum):
     STORE_CODE = os.getenv('KAFKA_TOPIC_STORE_CODE')
     PLAY_GAME = os.getenv('KAFKA_TOPIC_MATCH')
@@ -16,9 +32,10 @@ class Topics(enum.Enum):
 
 class KafkaClient:
     @staticmethod
-    def send_message(topic, message) -> bool:
+    def send_message(priority, message) -> bool:
         try:
-            kafka_producer.send(topic=topic, value=message)
+            arena = arenas[priority]
+            kafka_producer.send(topic=arena.topic_name, value=message, partition=arena.get_partition())
             kafka_producer.flush()
             return True
         except Exception as e:
